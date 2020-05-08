31017762905cb4570fafdb900da3d33a0d29d88c
Artem Zakharchenko <kettanaito@gmail.com>
2020-05-08 10:07:19 +0200

    Exports transformers, adds "types" to package

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ export { headersToList } from './headersToList'
 export { headersToObject } from './headersToObject'
 export { listToHeaders } from './listToHeaders'
 export { objectToHeaders } from './objectToHeaders'
+
+export { flattenHeadersList } from './flattenHeadersList'
+export { flattenHeadersObject } from './flattenHeadersObject'
