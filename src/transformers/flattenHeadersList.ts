4978d42b50316bd17244aa55b573565f1315fe21
Artem Zakharchenko <kettanaito@gmail.com>
2022-10-09 18:09:52 +0200

    fix(flattenHeadersList): join multiple values by comma

--- src/transformers/flattenHeadersList.ts
+++ src/transformers/flattenHeadersList.ts
@@ -2,6 +2,6 @@ import { HeadersList, FlatHeadersList } from '../glossary'
 
 export function flattenHeadersList(list: HeadersList): FlatHeadersList {
   return list.map(([name, values]) => {
-    return [name, ([] as string[]).concat(values).join('; ')]
+    return [name, ([] as string[]).concat(values).join(', ')]
   })
 }
